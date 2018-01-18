f6569766d4ac469a1d4ce107fb15ad20e304344a
jdobbi0346 <jasonq37@gmail.com>
2018-01-17 20:06:29 -0500

    Add project files.

---
+++ FoodCatering2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FoodCatering2.Startup))]
+namespace FoodCatering2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
